b9cb9eb679219eedcf35b39cfac8209bc86835b3
Natha0b <laura.barona04@gmail.com>
2022-11-16 21:15:18 -0500

    add function write

--- 0-printf.c
+++ 0-printf.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  *_printf- print a variable
@@ -19,49 +20,50 @@ int _printf(const char *format, ...)
 
 	va_start(arg, format);
 
-	for (i = 0; format[i]  != '\0'; i++)
+	if (!format)
+		return (0);
 
-	traverse = malloc(i + 1);
+	for (i = 0; format[i] != '\0'; i++)
+
+		traverse = malloc(i + 1);
 
 	if (traverse == NULL)
 	{
 		return (0);
 	}
-	for
 
-	while (*traverse != '\0')
-	{
-		_putchar(*traverse);
-		traverse++;
-
-		if ( *traverse != %)
+		while (*traverse != '\0')
 		{
-			_putchar(*traverse)
+			/*_putchar(*traverse);
 			traverse++;
+
+			/*if (*traverse != '%')
+			{
+				_putchar(*traverse);
+					traverse++;
+			}*/
+
+			switch (*traverse + 1)
+			{
+			case 'c':
+				c = (char)va_arg(arg, int);
+				write(1, &traverse, 1);
+				return (1);
+				break;
+			case 's':
+				s = va_arg(arg, char *);
+				write(1, &traverse, 1);
+				break;
+			case '%':
+				c = (char)va_arg(arg, int);
+				write(1, &traverse, 1);
+				break;
+
+			default:
+
+				continue;
+			}
 		}
-
-	switch (*traverse + 1)
-	{
-	case 'c':
-		c = (char)va_arg(arg, int);
-		_putchar(c);
-		break;
-	case 's':
-		s = va_arg(arg, char *);
-		puts(s);
-		break;
-	case '%':
-		% = (char)va_arg(arg, int);
-		_putchar(%);
-		break;
-	
-
-	default:
-
-		continue;
-	}
-	write
-	}
 
 	va_end(arg);
 	return (*traverse);
