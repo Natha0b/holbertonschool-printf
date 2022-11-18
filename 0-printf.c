141f5accc644d986bc3d5db37368e4100f3bb6ae
Natha0b <laura.barona04@gmail.com>
2022-11-17 22:02:45 -0500

    add function for case

--- 0-printf.c
+++ 0-printf.c
@@ -5,25 +5,24 @@
 #include <unistd.h>
 
 /**
- * _printf - print a variable
- * @format: variable printf
- * Return: the number of characters printed
+ *_printf- print a variable
+ *@format: variable printf
+ *Return: the number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	va_list arg;
+	va_list valist;
 	char *traverse = NULL;
 	int i, aux = 0;
 
-	va_start(arg, format);
+	va_start(valist, format);
 
 	if (!format)
 		return (0);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-
 	}
 
 	traverse = malloc(i + 1);
@@ -46,30 +45,48 @@ int _printf(const char *format, ...)
 
 		if (format[i] == '%')
 		{
-			while (format[i++] == ' ');
-			switch (format[i])
+			switch (format[i + 1])
 			{
 			case 'c':
-				function_char(arg);
+				function_char(valist);
 				aux++;
+				i++;
 				break;
-			/*case 's':
-				s = va_arg(arg, char *);
-				write(1, &s, 1);
+
+			case 'd':
+				function_int(valist);
+				aux++;
+				i++;
 				break;
-			case '%':
-				c = '%';
-				write(1, &c, 1);
-				break;*/
+
+			case 's':
+				function_s(valist);
+				aux++;
+				i++;
+				break;
+
+			case 'i':
+				function_int(valist);
+				aux++;
+				i++;
+				break;
+
+				/*
+				case '%':
+					c = '%';
+					write(1, &c, 1);
+					break;
+					*/
 
 			default:
 
 				continue;
 			}
+			aux++;
 		}
 		i++;
 	}
 
-	va_end(arg);
+	va_end(valist);
 	return (aux);
 }
--- printf_func.c
+++ printf_func.c
@@ -2,7 +2,7 @@
 
 /**
  * function_char - prints the char
- * @arg: args
+ * @valist: argument
  */
 int function_char(va_list valist)
 {
@@ -10,67 +10,36 @@ int function_char(va_list valist)
 	return (1);
 }
 
-
+/**
+ * function_int - prints the char
+ * @valist: argument
+ */
+int function_int(va_list valist)
+{
+	_putchar(va_arg(valist, int) + '0');
+	return (1);
+}
 
 /**
  * function_s - prints the integer
  * @arg: args
  */
-/*
-void function_s(va_list arg)
+
+void function_s(va_list valist)
 {
 	char *s;
+	int i;
 
-	s = va_arg(arg, char *);
-	_putchar("%s", s);
+	s = va_arg(valist, char *);
+	if (s == NULL)
+		s = ("(nil)");
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+
 }
-*/
-/**
- * function_percentage - prints the percent
- * @arg: arg
- */
-/*
-void function_percentage(va_list arg)
-{
-	_putchar("%%", va_arg(arg, int));
-}
-*/
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-/*
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-*/
-/**
- * _putchar - writes the character c to stdout
- * @s: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-/*
-int _putchar(char *s)
-{
-	return (write(1, &s, 1));
-}
-*/
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-/*
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-*/
+
+
+
+
